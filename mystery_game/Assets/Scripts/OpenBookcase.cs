1d6b7cea1009cd7859b79c1eeae8569b0e8d4657
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-03-16 21:17:47 +0000

    book and bookcase movement added

--- mystery_game/Assets/Scripts/OpenBookcase.cs
+++ mystery_game/Assets/Scripts/OpenBookcase.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class OpenBookcase : MonoBehaviour
 {
+    private Vector3 bookStartPosition;
+    private Vector3 bookEndPosition;
+    private bool bookSliding;
     private GameObject bookcase;
     private Vector3 bookcaseStartPosition;
     private Vector3 bookcaseEndPosition;
     private bool bookcaseSliding;
     private float timeElapsed;
-    private float lerpDuration = 3;
+    // private float lerpDuration = 3;
 
     // Start is called before the first frame update
     void Start() {
+        bookStartPosition = transform.position;
+        bookEndPosition = new Vector3(bookStartPosition.x, bookStartPosition.y, bookStartPosition.z + 0.1f);
+
         bookcase = transform.parent.gameObject;
         bookcaseStartPosition = bookcase.transform.position;
         bookcaseEndPosition = new Vector3(bookcaseStartPosition.x +1.5f, bookcaseStartPosition.y, bookcaseStartPosition.z);
     }
 
     private void OnMouseDown() {
-        Debug.Log("CLICK");
-        bookcaseSliding = true;
+        bookSliding = true;
+        // bookcaseSliding = true;
     }
 
     // Update is called once per frame
     void Update() {
+        if (bookSliding) {
+            if (timeElapsed < 1) {
+                float interpolationRatio = timeElapsed/1;
+                transform.position = Vector3.Lerp(bookStartPosition, bookEndPosition, interpolationRatio);
+                timeElapsed += Time.deltaTime;
+            } else {
+                bookSliding = false;
+                timeElapsed = 0;
+                bookcaseSliding = true;
+            }
+        }
+
         if (bookcaseSliding) {
-            if (timeElapsed < lerpDuration) {
-                float interpolationRatio = timeElapsed/lerpDuration;
+            if (timeElapsed < 2) {
+                float interpolationRatio = timeElapsed/2;
                 bookcase.transform.position = Vector3.Lerp(bookcaseStartPosition, bookcaseEndPosition, interpolationRatio);
                 timeElapsed += Time.deltaTime;
             } else {
