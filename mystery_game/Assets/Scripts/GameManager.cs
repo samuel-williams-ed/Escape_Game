08f9e25bc84f5587b1fb0989864ab4b8f0145f85
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-03-17 09:51:05 +0000

    further text script updates

--- mystery_game/Assets/Scripts/GameManager.cs
+++ mystery_game/Assets/Scripts/GameManager.cs
@@ -8,11 +8,9 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI dialogueText;
     private List<string> lines;
-    private int index; // to track where we are within the dialogue
-    private float textSpeed = 0.02f;
-    // public void UpdateDialogue(string newDialogue){
-    //     dialogueText.text = newDialogue;
-    // }
+    // private int index; // to track where we are within the dialogue
+    private float textSpeed = 0.15f;
+    // private bool dialogueInProgress;
 
     private List<string> getStartingText(){
         string string1 = "Where am I?";
@@ -29,7 +27,7 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-        index = 0;
+        // index = 0;
         lines = new List<string>();
         Debug.Log("start function running");
         // UpdateDialogue("...");
@@ -38,49 +36,86 @@ public class GameManager : MonoBehaviour
     }
     
     public void UpdateDialogue(List<string> newListOfStrings){
-        lines.AddRange(newListOfStrings);
+        // lines.AddRange(newListOfStrings);
+        lines = newListOfStrings;
         Debug.Log("current line =" + lines.Count);
-        StartCoroutine(TypeLine());
+        // StartCoroutine(TypeLine());
+        StartCoroutine(OutputDialogue());
     }
     //creating a co-routine
-    IEnumerator TypeLine(){
-        //Type each character one by one
-        foreach (char character in lines[index].ToCharArray()){ // takes the string and breaks it down to a character array
+    // IEnumerator TypeLine(){
+    //     //Type each character one by one
+    //     foreach (char character in lines[index].ToCharArray()){ // takes the string and breaks it down to a character array
         
-            dialogueText.text += character;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
+    //         dialogueText.text += character;
+    //         yield return new WaitForSeconds(textSpeed);
+    //     }
+    // }
 
-    void NextLine(){
-        if(index < lines.Count -1){
-            index++;
+    IEnumerator OutputDialogue() {
+        // Have a bool to track if dialogue already in progress?
+        // dialogueInProgress = true;
+
+        // Empty holding text from dialogue box:
+        dialogueText.text = string.Empty;
+
+        // Loop through each string in the lines array:
+        foreach (string line in lines) {
+            // Loop through each character in the string and print to dialogue box at given textSpeed:
+            foreach (char character in line.ToCharArray()) {
+                dialogueText.text += character;
+                yield return new WaitForSeconds(textSpeed);
+                if (Input.GetMouseButton(1)) { // If right-mouse button clicked, breaks loop and prints whole line
+                    break;
+                }
+            }
+            dialogueText.text = line;
+            yield return new WaitForSeconds(textSpeed * 5);
+            // Once all characters have been added, empty the dialogue text for next line:
             dialogueText.text = string.Empty;
-            StartCoroutine(TypeLine());
         }
-        else{
-            // lines.Clear();
-            lines = new List<string>();
-            index = 0;
-            dialogueText.text = "...";
-            // gameObject.SetActive(false); // gameObject refers to panel. 
 
-        }
+        // Reset lines variable to an empty array and add holding dialogue text:
+        lines = new List<string>();
+        dialogueText.text = "...";
+
+        // dialogueInProgress = false;
     }
 
-        // Update is called once per frame
+    // void NextLine(){
+    //     if(index < lines.Count -1){
+    //         index++;
+    //         dialogueText.text = string.Empty;
+    //         StartCoroutine(TypeLine());
+    //     }
+    //     else{
+    //         // lines.Clear();
+    //         lines = new List<string>();
+    //         index = 0;
+    //         dialogueText.text = "...";
+    //         // gameObject.SetActive(false); // gameObject refers to panel. 
+
+    //     }
+    // }
+
+    // Update is called once per frame
     void Update(){
-        if(Input.GetMouseButtonDown(0)){ // 0 is the left button
 
-            if(dialogueText.text == lines[index]){
-                NextLine();
-            }
-            else{
-                StopAllCoroutines();
+        // USING MOUSE CLICK TO COMPLETE TEXT APPEARING FASTER WON'T WORK ALONGSIDE HAVING TEXT APPEAR ONCLICK - 
+        // AS CLICKING ON OBJECT WILL ALSO DOUBLE UP AS MAKING TEXT FULLY APPEAR SO NO TYPEWRITER EFFECT?
+        // COULD WE USE ANOTHER KEY TO SKIP TEXT TO END?
 
-                dialogueText.text = lines[index];   // this will get the current line and instantly fill it out
-            }
-        }
+        // if(Input.GetMouseButtonDown(0)){ // 0 is the left button
+
+        //     if(dialogueText.text == lines[index]){
+        //         NextLine();
+        //     }
+        //     else{
+        //         StopAllCoroutines();
+
+        //         dialogueText.text = lines[index];   // this will get the current line and instantly fill it out
+        //     }
+        // }
     }
 }
 
--- mystery_game/Assets/Scripts/TextOnlyObjectsScript.cs
+++ mystery_game/Assets/Scripts/TextOnlyObjectsScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextOnlyObjectsScript : MonoBehaviour
 
