83c26889cd13a956dfa3607305f080c70e6326b0
Char Broadley <broadleychar@gmail.com>
2023-03-17 22:41:31 +0000

    drawer one opening and closing with dialogue but having issues having an onclick on the book too

---
+++ mystery_game/Assets/Scripts/ChestOfDrawers/DrawerOneBook.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerOneBook : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+}
