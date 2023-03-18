99afa3bb32c31c864e9d13f021ade055fe42184b
Char Broadley <broadleychar@gmail.com>
2023-03-18 18:09:22 +0000

    SCALES WORKINGGGG

--- mystery_game/Assets/Scripts/OpenDrawerScales.cs
+++ mystery_game/Assets/Scripts/OpenDrawerScales.cs
@@ -6,31 +6,35 @@ public class OpenDrawerScales : MonoBehaviour
 {
     private Vector3 drawerStartPosition;
     private Vector3 drawerEndPosition;
-    private bool drawerOpen;
+    // private bool drawerOpen;
     private float timeElapsed;
     //private GameObject chestOfDrawers;
     private GameObject topDrawerTwo;
+    private bool drawerSliding;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        // aFinger = transform.Find("LeftShoulder/Arm/Hand/Finger");
-        // topDrawerTwo = transform.Find("TopDrawer2");
-        //chestOfDrawers = GameObject.Find("ChestOfDrawers");
-        //topDrawerTwo = GameObject.Find("TopDrawer2");
-        // GameObject go = GameObject.FindGameObjectsWithTag("Player");
-        topDrawerTwo = GameObject.FindGameObjectsWithTag("SecretDrawer");
+    void Start() {
+        topDrawerTwo = GameObject.FindGameObjectsWithTag("SecretDrawer")[0];
     }
 
     private void OnMouseDown() {
-        Debug.Log("CLICK");
         drawerStartPosition = topDrawerTwo.transform.position;
-        if (drawerOpen) {
-            drawerEndPosition = new Vector3(drawerStartPosition.x + 0.5f, drawerStartPosition.y, drawerStartPosition.z);
-            drawerOpen = false;
+        drawerEndPosition = new Vector3(drawerStartPosition.x + 0.5f, drawerStartPosition.y, drawerStartPosition.z);
+        drawerEndPosition = new Vector3(drawerStartPosition.x - 0.5f, drawerStartPosition.y, drawerStartPosition.z);
+        GameManager.manager.setSecretDrawerUnlocked(true);
+        drawerSliding = true;  
+    }
+
+    // Update is called once per frame
+    void Update() {
+    if (drawerSliding) {
+        if (timeElapsed < 1) {
+            topDrawerTwo.transform.position = Vector3.Lerp(drawerStartPosition, drawerEndPosition, timeElapsed);
+            timeElapsed += Time.deltaTime;
         } else {
-            drawerEndPosition = new Vector3(drawerStartPosition.x - 0.5f, drawerStartPosition.y, drawerStartPosition.z);
-            drawerOpen = true;
-        }   
+            drawerSliding = false;
+            topDrawerTwo.transform.position = drawerEndPosition;
+            }
+        }
     }
 }
