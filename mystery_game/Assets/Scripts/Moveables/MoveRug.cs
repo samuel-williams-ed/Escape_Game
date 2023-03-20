342b8a74849efdba84ed21e189a139404f9f08de
Samuel Williams <samuel.williams.ed@gmail.com>
2023-03-20 22:15:11 +0000

    conditional to check player is close. Player start position reset to 0 coordinates and orientation

--- mystery_game/Assets/Scripts/Moveables/MoveRug.cs
+++ mystery_game/Assets/Scripts/Moveables/MoveRug.cs
@@ -13,11 +13,15 @@ public class MoveRug : MonoBehaviour {
     public void moveRug(GameObject rug_to_move){
 
         // if player position.z is > 2.2
-        
-        gameObject.transform.Rotate(0, -25, 0);
-        gameObject.transform.position = new Vector3(-1.5f, 0.15f, 3f);
+        if (GameManager.manager.getPlayer().transform.position.z > 2.2 ){
 
-        Debug.Log("Moving rug " + gameObject.name);
-        GameManager.manager.UpdateDialogue(new List<string>(){"What's this image hidden under the rug... must be important..."});
+            gameObject.transform.Rotate(0, -25, 0);
+            gameObject.transform.position = new Vector3(-1.5f, 0.15f, 3f);
+
+            Debug.Log("Moving rug " + gameObject.name);
+            GameManager.manager.UpdateDialogue(new List<string>(){"What's this image hidden under the rug... must be important..."});
+
+        }
+        
     }
 }
