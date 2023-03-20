99978ead73ca927a7ce2554ef452c3059a2a842d
Samuel Williams <samuel.williams.ed@gmail.com>
2023-03-20 14:56:00 +0000

    Secret door opens to correct position and persists

--- mystery_game/Assets/Scripts/Moveables/OpenSecretDoor.cs
+++ mystery_game/Assets/Scripts/Moveables/OpenSecretDoor.cs
@@ -7,10 +7,12 @@ public class OpenSecretDoor : MonoBehaviour
 
     void Start() {
         if ( PlayerInventory.manager.askIfSecretDoorOpened() ){
-            
-            gameObject.transform.rotation = new Quaternion(0f, -90f, 0f, 0f);
-            gameObject.transform.position.x = 
+            Debug.Log("Secret door has been opened!");
+            // gameObject.transform.rotation = new Quaternion(0f, -90f, 0f, 0f);
+            gameObject.transform.Rotate(0, 90, 0);
+            gameObject.transform.position = new Vector3(1.01f, 0.085f, -3.678f);
+            // original position is (0.458f, 0.085f, -3.11f)
         }
-}
+    }
 
 }
