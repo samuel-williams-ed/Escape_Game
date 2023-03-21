632adac5c5986407f4ac05948423127fc93b163c
Samuel Williams <samuel.williams.ed@gmail.com>
2023-03-21 18:04:42 +0000

    minor edits

--- mystery_game/Assets/Scripts/Moveables/OpenSecretDoor.cs
+++ mystery_game/Assets/Scripts/Moveables/OpenSecretDoor.cs
@@ -9,10 +9,8 @@ public class OpenSecretDoor : MonoBehaviour
         if ( PlayerInventory.manager.askIfSecretDoorOpened() ){
             Debug.Log("Secret door has been opened!");
             
-            // gameObject.transform.rotation = new Quaternion(0f, -90f, 0f, 0f);
             gameObject.transform.Rotate(0, 90, 0);
             gameObject.transform.position = new Vector3(1.01f, 0.085f, -3.678f);
-            // original position is (0.458f, 0.085f, -3.11f)
         }
     }
 
