2f7fd19c87133e61d75d8d64420cab6f56e507f0
Samuel Williams <samuel.williams.ed@gmail.com>
2023-03-22 01:26:24 +0000

    fixed (kinda) player disorientation after opening secret bookcase

--- mystery_game/Assets/Scripts/SecretDoor/UnlockBlue.cs
+++ mystery_game/Assets/Scripts/SecretDoor/UnlockBlue.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UnlockBlue : MonoBehaviour {
 
+    public GameObject player;
+
 
 public GameObject secret_door; // it receives the secret_door object as a parameter (assigned in heirarchy)
 
@@ -45,7 +47,45 @@ public GameObject secret_door; // it receives the secret_door object as a parame
             // Debug.Log("Door Opened!");
             GameManager.manager.UpdateDialogue(new List<string>(){"The door opened...", "Look there's a secret room!"});
 
+            // StartCoroutine(reorientatePlayer());
+
+            player = GameManager.manager.getPlayer();
+            player.transform.localRotation = new Quaternion(0f, 176f, 0f, 0f);
+
             ScenesManager.manager.LoadMainRoom();
+            PlayerMove.manager.setPlayerMoveable(true);
+            PlayerLook.manager.setPlayerCanMoveCamera(true);
+        }
+    }
+
+
+
+    private IEnumerator reorientatePlayer() {
+
+        player = GameManager.manager.getPlayer();
+            
+            // get current player position & orientation
+            Vector3 currentPosition;
+            Quaternion currentRotation;
+            player.transform.GetLocalPositionAndRotation(out currentPosition, out currentRotation);
+
+            // reset orientation - keep position
+            //player.transform.SetLocalPositionAndRotation(currentPosition, new Quaternion(0f,175f,0f,0f));
+
+        // Following moves player over time period of 1 second:
+        float timeElapsed = 0;
+        while (timeElapsed < 1) {
+
+            // Move & rotate player:
+            player.transform.localPosition = Vector3.Lerp(currentPosition, currentPosition, timeElapsed);
+            player.transform.localRotation = Quaternion.Slerp(currentRotation, new Quaternion(0, 0, 0, 0), timeElapsed);
+            
+            // reset camera orientation - may not be neccesary
+            // Camera.main.transform.SetLocalPositionAndRotation(new Vector3(0f, 0.5f, 0f), new Quaternion(0, 0, 0, 0));
+
+            // Update timeElapsed variable:
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
     }
 }
